1200e96c43e00a8e01f4e33b8831eac5eba025c3
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-11-19 14:10:45 +0100

    refactor: move to vitest

--- tests/randomString.test.ts
+++ tests/randomString.test.ts
@@ -1,4 +1,5 @@
 import { randomString } from '../src/index.ts'
+import { describe, it, expect } from 'vitest'
 
 describe('randomString', () => {
     it('should return a random string with the length of 10', () => {
